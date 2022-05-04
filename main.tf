0607b634962abd7e49d02a13f842fe291e03a625
jbenaventem <jesus.benavente@gruposantander.com>
2022-05-04 08:58:13 +0200

    change to aws_s3_bucket_acl

--- main.tf
+++ main.tf
@@ -6,9 +6,7 @@ resource "random_id" "tf_bucket_id" {
 
 # Create the bucket
 resource "aws_s3_bucket" "tf_code" {
-  bucket = var.project_name
-  acl    = "private"
-
+  bucket        = var.project_name
   force_destroy = true
 
   tags = {
@@ -16,3 +14,7 @@ resource "aws_s3_bucket" "tf_code" {
   }
 }
 
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.tf_code.id
+  acl    = "private"
+}
